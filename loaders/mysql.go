18fafbc78a549b82a3e7f62b6fe752122d16dc92
tyrone <2125081@qq.com>
2017-09-02 11:21:30 +0800

    bind pointer variable if column is nullable

--- loaders/mysql.go
+++ loaders/mysql.go
@@ -100,66 +100,66 @@ switchDT:
 		nilVal = "0"
 		if precision == 1 {
 			nilVal = "false"
-			typ = "*bool"
+			typ = "bool"
 			break switchDT
 		} else if precision <= 8 {
-			typ = "*uint8"
+			typ = "uint8"
 		} else if precision <= 16 {
-			typ = "*uint16"
+			typ = "uint16"
 		} else if precision <= 32 {
-			typ = "*uint32"
+			typ = "uint32"
 		} else {
-			typ = "*uint64"
+			typ = "uint64"
 		}
 
 	case "bool", "boolean":
 		nilVal = "false"
-		typ = "*bool"
+		typ = "bool"
 
 	case "char", "varchar", "tinytext", "text", "mediumtext", "longtext":
 		nilVal = `""`
-		typ = "*string"
+		typ = "string"
 
 	case "tinyint":
 		//people using tinyint(1) really want a bool
 		if precision == 1 {
 			nilVal = "false"
-			typ = "*bool"
+			typ = "bool"
 			break
 		}
 		nilVal = "0"
-		typ = "*int8"
+		typ = "int8"
 
 	case "smallint":
 		nilVal = "0"
-		typ = "*int16"
+		typ = "int16"
 
 	case "mediumint", "int", "integer":
 		nilVal = "0"
-		typ = "*int32"
+		typ = args.Int32Type
 
 	case "bigint":
 		nilVal = "0"
-		typ = "*int64"
+		typ = "int64"
 
 	case "float":
 		nilVal = "0.0"
-		typ = "*float32"
+		typ = "float32"
 
 	case "decimal", "double":
 		nilVal = "0.0"
-		typ = "*float64"
+		typ = "float64"
 
 	case "binary", "varbinary", "tinyblob", "blob", "mediumblob", "longblob":
-		typ = "*[]byte"
+		typ = "[]byte"
 
 	case "timestamp", "datetime", "date":
 		nilVal = "time.Time{}"
-		typ = "*time.Time"
+		typ = "time.Time"
 
 	case "time":
 		// time is not supported by the MySQL driver. Can parse the string to time.Time in the user code.
-		typ = "*string"
+		typ = "string"
 
 	default:
 		if strings.HasPrefix(dt, args.Schema+".") {
@@ -175,8 +175,12 @@ switchDT:
 	// add 'u' as prefix to type if its unsigned
 	// FIXME: this needs to be tested properly...
 	if unsigned && internal.IntRE.MatchString(typ) {
-		typ = "*u" + typ
+		typ = "u" + typ
 	}
+
+	if nullable {
+		typ = "*" + typ
+        }
 
 	return precision, nilVal, typ
 }
